ed1e188ae5258e7051030b4f5c787b7ea1485d39
Jay1617 <jaypatel16617@gmail.com>
2025-01-21 21:51:48 +0530

    Enhance user registration by adding phone number and verification method

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -3,17 +3,45 @@ import jwt from "jsonwebtoken";
 import ErrorHandler from "../utils/errorHandler.js";
 import { catchAsyncError } from "../utils/catchAsyncError.js";
 
-export const userRegister = async (req, res) => {
+export const register = catchAsyncError(async (req, res, next) => {
   try {
-    const { name, email, password, role } = req.body;
+    const { name, email, password, phone, verificationMethod } = req.body;
+
+    if (!name || !email || !password || !phone || !verificationMethod) {
+      return next(new ErrorHandler("Please fill in all fields", 400));
+    }
+
+    function validatePhone(phone) {
+      const phoneRegex = /^(?:\+91)?[6-9][0-9]{9}$/;
+      return phoneRegex.test(phone);
+    }
+
+    if (!validatePhone(phone)) {
+      return next(new ErrorHandler("Please enter a valid phone number", 400));
+    }
+
+    const existingUser = await User.findOne({
+      $or: [
+        { email, accountVerified: true },
+        { phone, accountVerified: true },
+      ],
+    });
+
     const user = await User.create({
       name,
       email,
       password,
-      role,
+      avatar: {
+        public_id: "avatars/no-avatar.jpg",
+        url: "https://res.cloudinary.com/df9jsefb9/image/upload/v1627314537/avatars/no-avatar.jpg",
+      },
     });
-    sendToken(user, 201, res);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
+
+    const token = user.getJwtToken();
+
+    res.status(201).json({
+      success: true,
+      token,
+    });
+  } catch (error) {}
+});
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -4,69 +4,81 @@ import jwt from "jsonwebtoken";
 import validator from "validator";
 
 const userSchema = new mongoose.Schema(
-    {
-        username: {
-            type: String,
-            required: true,
-            minLength: [3,"Name must contain at least 3 characters!"],
-            maxLength: [16, "Name cannot exceed 32 characters"],
-        },
-        email: {
-            type: String,
-            required: true,
-            validate: [validator.isEmail, "Please provide a valid email!"],
-        },
-        avatar:{
-            public_id:{
-                type: String,
-                required: true
-            },
-            url:{
-                type: String,
-                required: true
-            },
-        },
-        password: {
-            type: String,
-            required: true,
-            minLength: [8,"Password must contain at least 8 characters!"],
-            maxLength: [32, "Password cannot exceed 32 characters"],
-            select: false,
-        },
-        resetPasswordToken: String,
-        resetPasswordExpire: Date,
-        verifyEmailToken: String,
-        verifyEmailTokenExpire: Date,
-        isEmailVerified: {
-            type: Boolean,
-            default: false,
-        },
-        createdOn: {
-            type: Date,
-            default: Date.now,
-        },
+  {
+    username: {
+      type: String,
+      required: true,
+      minLength: [3, "Name must contain at least 3 characters!"],
+      maxLength: [16, "Name cannot exceed 32 characters"],
     },
-    {
-        timestamps: true,
-    }
+    email: {
+      type: String,
+      required: true,
+      validate: [validator.isEmail, "Please provide a valid email!"],
+    },
+    avatar: {
+      public_id: {
+        type: String,
+        required: true,
+      },
+      url: {
+        type: String,
+        required: true,
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: [8, "Password must contain at least 8 characters!"],
+      maxLength: [32, "Password cannot exceed 32 characters"],
+      select: false,
+    },
+    phone: {
+      type: String,
+      required: [true, "Please enter your phone number"],
+      maxLength: [13, "Phone number cannot exceed 13 characters"],
+    },
+    verificationMethod: {
+      type: String,
+      required: true,
+      enum: {
+        values: ["email", "phone"],
+        message: "Please select a verification method",
+      },
+    },
+    verificationCode: Number,
+    verificationCodeExpires: Date,
+    resetPasswordToken: String,
+    resetPasswordExpires: Date,
+    accountVerified: {
+      type: Boolean,
+      default: false,
+    },
+    createdOn: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
 userSchema.pre("save", async function (next) {
-    if (!this.isModified("password")) {
-        next();
-    }
-    this.password = await bcrypt.hash(this.password, 10);
-    
+  if (!this.isModified("password")) {
+    next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
 });
 
 userSchema.methods.comparePassword = async function (enteredPassword) {
-    return await bcrypt.compare(enteredPassword, this.password);
+  return await bcrypt.compare(enteredPassword, this.password);
 };
 
 usderSchema.methods.getJwtToken = function () {
-    return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
-        expiresIn: process.env.JWT_EXPIRES_TIME,
-    });
-}
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_TIME,
+  });
+};
 
-export const User =  mongoose.model("User", userSchema);
+export const User = mongoose.model("User", userSchema);
