b2b69515b32b047839c0bb45444ffc819ab43445
Jay1617 <jaypatel16617@gmail.com>
2025-01-27 20:20:55 +0530

    Adding frontend folder

--- backend/automation/removeUnverifiedUser.js
+++ backend/automation/removeUnverifiedUser.js
@@ -1,18 +1,12 @@
 import cron from "node-cron";
 import { User } from "../models/user.model.js";
 
-export const removeUnverifiedUser = () =>
-  cron.schedule(
-    "0 0 * * *",
-    async () => {
-      const thirtyMinTime = new Date(Date.now() - 30 * 60 * 1000);
-      await User.deleteMany({
-        accountVerified: false,
-        createdOn: { $lt: thirtyMinTime },
-      });
-    },
-    {
-      scheduled: true,
-      timezone: "Asia/Kolkata",
-    }
-  );
+export const removeUnverifiedUser = () =>{
+  cron.schedule("*/30 * * * *", async () => {
+    const thirtyMinutesAgo = new Date(Date.now() - 30 * 60 * 1000);
+    await User.deleteMany({
+      accountVerified: false,
+      createdAt: { $lt: thirtyMinutesAgo },
+    });
+  });
+};
